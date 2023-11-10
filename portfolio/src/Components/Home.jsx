496c6098edfd106ab3bf0c4a4664ef81c73942dd
Matu36 <matipineda85@live.com.ar>
2023-11-10 01:32:03 -0300

    js

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -32,6 +32,35 @@ export default function Home() {
     });
   }, []);
 
+  const biografia = `
+const biografia = [
+  {
+    Título: "Inicio en el Desarrollo Web",
+    Descripción:
+      "Actualmente me desempeño en el área del desarrollo web, tanto Backend como Frontend. Inicié mi carrera en este emocionante campo con un fuerte interés en construir soluciones tecnológicas.",
+    Año: 2022, // Reemplaza con el año correspondiente
+  },
+  {
+    Título: "Pasión por el Trabajo en Equipo",
+    Descripción:
+      "Me apasiona el trabajo en equipo, donde todos compartimos un mismo objetivo y nos desempeñamos arduamente en pos de conseguirlo. Creo firmemente en la colaboración y la sinergia para lograr resultados excepcionales.",
+  },
+  {
+    Título: "Explorando Nuevas Tecnologías",
+    Descripción:
+      "El mundo de la programación es increíble; nada me divierte más que seguir aprendiendo tecnologías nuevas e implementarlas con el fin de alcanzar mis metas. Siempre estoy en busca de desafíos y oportunidades para mejorar mis habilidades.",
+  },
+];
+`;
+
+  const Biography = () => {
+    return (
+      <Box>
+        <Text whiteSpace="pre-wrap">{biografia}</Text>
+      </Box>
+    );
+  };
+
   return (
     <Box>
       <NavBar onToggleSidebar={toggleSidebar} />
@@ -54,10 +83,19 @@ export default function Home() {
       </Flex>
 
       <Box className="personal">
-        <Text fontSize="30px" fontWeight="bold" marginLeft="1rem">
+        <Text
+          fontSize="30px"
+          fontWeight="bold"
+          marginLeft="1rem"
+          color="#2B6388"
+        >
           Mis Datos Personales
         </Text>
         <img src={personal} alt="personal" />
+
+        <Text>
+          <Biography />
+        </Text>
       </Box>
     </Box>
   );
