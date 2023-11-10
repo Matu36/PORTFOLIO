86df26bfa4d8b833962812fed7fe6e8b84a4b1b7
Matu36 <matipineda85@live.com.ar>
2023-11-10 01:19:29 -0300

    port

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import { Text, Box, Button, Image, Flex } from "@chakra-ui/react";
-import CV from "../Utils/CV.ACTUALIZADO.pdf";
-import countries from "../img/PAISESNUEVO.png";
-import palitos from "../img/Palitos.jpg";
 import { Icon, Link } from "@chakra-ui/react";
 import { FaGithub, FaGlobe, FaLinkedin, FaEnvelope } from "react-icons/fa";
 import mangiare from "../img/mangiare.png";
@@ -12,6 +9,7 @@ import ECOMMERCE from "../img/buenProvechoNuevo.png";
 import Carrousel from "../Components/Styles/Carrousel";
 import SideBar from "./SideBar";
 import { toast } from "react-toastify";
+import personal from "../img/POSTMAN.png";
 
 export default function Home() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -38,8 +36,28 @@ export default function Home() {
     <Box>
       <NavBar onToggleSidebar={toggleSidebar} />
       <SideBar isOpen={isSidebarOpen} />
-      <Box>
-        <img src={matu} alt="matu" className="matu" />
+
+      <Flex justifyContent="space-evenly" marginLeft="-40rem">
+        <Box>
+          <img src={matu} alt="matu" className="matu" />
+        </Box>
+
+        <Box marginTop="15rem">
+          <Text fontSize="50px" fontWeight="bold">
+            {" "}
+            Matías Pineda
+          </Text>
+          <Text fontSize="30px" color="yellow.400" fontWeight="bold">
+            Soy un desarrollador Full Stack!
+          </Text>
+        </Box>
+      </Flex>
+
+      <Box className="personal">
+        <Text fontSize="30px" fontWeight="bold" marginLeft="1rem">
+          Mis Datos Personales
+        </Text>
+        <img src={personal} alt="personal" />
       </Box>
     </Box>
   );
